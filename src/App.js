feda196e206e67a71e0c386305fa85b1aa70bd4b
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-08-24 16:48:52 -0300

    59 - Flexible Lists

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: '1', name: 'Max', age: 28 },
+      { id: '2', name: 'Manu', age: 29 },
+      { id: '3', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -24,8 +24,10 @@ class App extends Component {
   }
 
   togglePersonsHandler = () => {
+    console.log(this.state.showPersons);
     const doesShow = this.state.showPersons;
     this.setState( { showPersons: !doesShow } );
+    console.log(this.state.showPersons);
   }
 
   deletePersonHandler = (personIndex) => {
@@ -52,7 +54,8 @@ class App extends Component {
             return <Person
                       click={() => this.deletePersonHandler(index)}
                       name={person.name}
-                      age={person.age} />
+                      age={person.age} 
+                      key={person.id} />
           })}
          
         </div>
