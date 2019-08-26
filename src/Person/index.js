471e23d894e013b19e450705dd290cf60f8952ed
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-08-26 13:15:13 -0300

    Removing Radiun

--- src/Person/index.js
+++ src/Person/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import Radium from 'radium';
 import './styles.css';
 
 const person = ( props ) => {
-  const style = {
-    '@media (min-width: 500px)': {
-      width: '450px'
-    }
-  }
   return (
-    <div className="person" style={style}>
+    <div className="person">
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
@@ -23,4 +17,4 @@ const person = ( props ) => {
   )
 };
 
-export default Radium(person);
+export default person;
