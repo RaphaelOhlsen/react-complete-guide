b538cb6291fb4ad6b05e527a1a596bc41115ca85
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-08-27 18:08:39 -0300

    Using useEffect() in Funcional COmponents

--- src/components/Cockpit/index.js
+++ src/components/Cockpit/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import classes from './styles.css';
 
 const cockpit = (props) => {
+
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+  })
 
   const assignedClasses = [];
   let btnClass = '';
