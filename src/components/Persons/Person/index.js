ccb7ca6404ccbef9d5a036c29d81f38a7e04785f
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-08-27 17:58:20 -0300

    Component Update Lificycle

--- src/components/Persons/Person/index.js
+++ src/components/Persons/Person/index.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './styles.css';
 
-const person = ( props ) => {
-  return (
-    <div className={classes.person}>
-      <p onClick={props.click}>
-        I'm {props.name} and I am {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input 
-        type="text" 
-        onChange={props.changed} 
-        value={props.name} 
-      />
-    </div>
-  )
+class Person extends Component { 
+  
+  
+  render() {
+    console.log('[Person.js] rendering...');
+    return (
+      <div className={classes.person}>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input 
+          type="text" 
+          onChange={this.props.changed} 
+          value={this.props.name} 
+        />
+      </div>
+    )
+  }
+  
 };
 
-export default person;
+export default Person;
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,12 @@ import Persons from '../components/Persons'
 import Cockpit from '../components/Cockpit';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: '1', name: 'Max', age: 28 },
@@ -11,8 +17,31 @@ class App extends Component {
       { id: '3', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
-    showPersons: true
+    showPersons: false
   }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  // componentWillMount() {
+  //   console.log('[App.js] componentWillMount');
+  // }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  };
+
+  shouldComponentUpdate(nextProps, nextStatte) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
+
 
   nameChangedHandler = ( event, id ) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -42,6 +71,7 @@ class App extends Component {
   };
 
   render () {
+    console.log('[App.js] render');
     let persons = null;
 
     if ( this.state.showPersons ) {
@@ -55,6 +85,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit 
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}
